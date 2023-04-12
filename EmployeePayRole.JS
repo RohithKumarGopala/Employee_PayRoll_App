db671c7298f060b7b9f027ae27a6248552baff5e
“rohithgopala44” <rohithgopala44@gmail.com>
2023-04-12 08:05:23 +0530

    Added: Program Of Saving Object Data To Local Storage

--- EmployeePayRole.JS
+++ EmployeePayRole.JS
@@ -64,8 +64,8 @@ class EmployeePayrollData{
         const options = { year: 'numeric', month: 'long', day: 'numeric'};
         const empDate = !this.startDate ? "undefined":
                           this.startDate.toLocaleDateString("en-US", options);
-        return "id=" +this.id + ", name=" +this.name + ",gender=" +this.gender +
-               ",profilePic=" + this.profilePic + ", department=" + this.department +
+        return "id=" +this.id + ", name='" +this.name + ",gender='" +this.gender +
+               ",profilePic='" + this.profilePic + ", department=" + this.department +
                ",salary=" + this.salary + ", startDate=" + empDate+ ",note=" + this.note;
         }
     }
